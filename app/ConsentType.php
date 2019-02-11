c59fd0d3c90e89b3bfe345cb961304055b1fa082
Andrew Drake <andrew.drake@websurfermedia.com>
2019-02-11 13:40:52 +1100

    Initial models

---
+++ app/ConsentType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\Model;
+
+class ConsentType extends Model
+{
+    //
+}
---
+++ app/Model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class Model extends EloquentModel
+{
+
+    protected $guarded = [];
+
+}
