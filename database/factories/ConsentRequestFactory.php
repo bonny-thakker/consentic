ada99da24b409c8ad0403915a15d869c6853a4f6
Andrew Drake <andrew.drake@websurfermedia.com>
2019-02-21 08:56:06 +1100

    Fixed seeder

--- database/factories/ConsentRequestFactory.php
+++ database/factories/ConsentRequestFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\ConsentRequest::class, function (Faker $faker) {
     return [
+        'team_id' => 1,
         'consent_id' => \App\Consent::all()->random(1)->first()->id,
         'user_id' => $faker->randomElement([1 ,2, 3]),
         'patient_id' => \App\Patient::all()->random(1)->first()->id,
