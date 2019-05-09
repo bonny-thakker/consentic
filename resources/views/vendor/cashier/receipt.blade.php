7a802019eafd6004b8cd8b824da5101a173abc97
Andrew Drake <andrew.drake@websurfermedia.com>
2019-05-09 10:19:08 +1000

    Invoice testing

--- resources/views/vendor/cashier/receipt.blade.php
+++ resources/views/vendor/cashier/receipt.blade.php
@@ -148,7 +148,10 @@
                         <!-- Display The Subscriptions -->
                             @foreach ($invoice->subscriptions() as $subscription)
                                 <tr>
-                                    <td>{{ print_r($subscription->asStripeInvoiceItem()) }}</td>
+                                    <td>
+                                        {{ $subscription->asStripeInvoiceItem() }}
+                                        Plan: {{ $subscription->asStripeInvoiceItem()->metadata->plan->metadata->nickname ?? null }}
+                                    </td>
                                     <td>
                                         {{ $subscription->startDateAsCarbon()->formatLocalized('%B %e, %Y') }} -
                                         {{ $subscription->endDateAsCarbon()->formatLocalized('%B %e, %Y') }}
