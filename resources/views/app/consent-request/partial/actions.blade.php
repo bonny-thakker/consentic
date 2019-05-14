56d476d9ed250bbea13694582c1360bef383dbf9
Andrew Drake <andrew.drake@websurfermedia.com>
2019-05-14 16:14:10 +1000

    Doctor complete button

--- resources/views/app/consent-request/partial/actions.blade.php
+++ resources/views/app/consent-request/partial/actions.blade.php
@@ -6,7 +6,7 @@
             </a>
         </p>
         <p class="control">
-            @if($consentRequest->isPatientSigned())
+            @if($consentRequest->isPatientSigned() && $consentRequest->isUserSigned())
                 <a class="button is-secondary tooltip " href="/app/consent-requests/{{ $consentRequest->id }}/download" data-tooltip="Download" data-id="">
                     <i class="fas fa-file-alt"></i>
                 </a>
@@ -20,6 +20,17 @@
             <a class="button is-warning has-text-white tooltip action-edit" data-tooltip="Edit" href="{{ ($consentRequest->isPatientSigned()) ? '#' : url('app/consent-requests/'.$consentRequest->id.'/edit') }}" {{ ($consentRequest->isPatientSigned()) ? 'disabled="disabled"' : null }}>
                 <i class="fas fa-edit"></i>
             </a>
+        </p>
+        <p class="control">
+            @if($consentRequest->isPatientSigned() && !$consentRequest->isUserSigned())
+            <a class="button is-success tooltip" data-tooltip="Doctor Complete" href="/app/consent-requests/{{ $consentRequest->id }}/doctor-questions/edit" >
+                <i class="fas fa-clipboard-check"></i>
+            </a>
+            @else
+                <a class="button is-success tooltip" data-tooltip="Doctor Complete" href="#" disabled="" >
+                    <i class="fas fa-clipboard-check"></i>
+                </a>
+            @endif
         </p>
         <p class="control">
             <a class="button is-danger tooltip {{ ($consentRequest->isPatientSigned()) ?  : 'action-delete delete-button' }} " data-tooltip="Delete" {{ ($consentRequest->isPatientSigned()) ? 'disabled="disabled"' : null }} >
